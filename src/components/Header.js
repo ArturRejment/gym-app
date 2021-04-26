375d759c9863bcaba20b70e80030f008d34d373d
kamilkow1123 <22kamil.kow@gmail.com>
2021-04-26 15:25:28 +0200

    Responsive landing page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "./Link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "../style/header.css";
+import useWindowSize from "../hooks/useWindowSize";
 
 const Header = () => {
-  return (
-    <div className="navbar">
-      <Link href="/" className="item">
-        GymFit
-      </Link>
-      <div className="toggle-button">|||</div>
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const { height, width } = useWindowSize();
+
+  let menu;
+  let hamburger;
+  let mobileMenu;
+  let mobileMenuMask;
+
+  if (width >= 768) {
+    // setShowMobileMenu(false);
+    menu = (
       <div className="nav-menu">
         <Link href="/trainer" className="item">
           Trainer
@@ -20,7 +28,50 @@ const Header = () => {
           Client
         </Link>
       </div>
-    </div>
+    );
+  } else {
+    hamburger = (
+      <div className="toggle-button">
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+        />
+      </div>
+    );
+
+    if (showMobileMenu) {
+      mobileMenu = (
+        <div className="nav-mobile-menu">
+          <Link href="/trainer" className="item-mobile">
+            Trainer
+          </Link>
+          <Link href="/employee" className="item-mobile">
+            Employee
+          </Link>
+          <Link href="/client" className="item-mobile">
+            Client
+          </Link>
+        </div>
+      );
+      mobileMenuMask = (
+        <div
+          className="mobile-menu-mask"
+          onClick={() => setShowMobileMenu(false)}
+        ></div>
+      );
+    }
+  }
+
+  return (
+    <header className="navbar">
+      <Link href="/" className="item">
+        GymFit
+      </Link>
+      {mobileMenuMask}
+      {mobileMenu}
+      {hamburger}
+      {menu}
+    </header>
   );
 };
 
