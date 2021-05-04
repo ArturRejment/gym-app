8dfd35c5f9946803afb923131d53ebc7606a4be9
kamilkow1123 <22kamil.kow@gmail.com>
2021-05-04 22:42:28 +0200

    Connecting React with Django API

--- React/src/components/UserPage.js
+++ React/src/components/UserPage.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React from 'react';
+import useResults from '../hooks/useResults';
 
 function UserPage() {
-  return <div>Hello</div>;
+	const products = useResults('Products');
+	const members = useResults('GymMember');
+	const trainers = useResults('Trainer');
+
+	const renderedProducts = products.map((product) => {
+		return <div key={product.product_id}>{product.product_name}</div>;
+	});
+
+	const renderedMembers = members.map((member) => {
+		return (
+			<div key={member.member_id}>
+				{member.first_name} {member.last_name}
+			</div>
+		);
+	});
+
+	const renderedTrainers = trainers.map((trainer) => {
+		return (
+			<div key={trainer.trainer_id}>
+				{trainer.first_name} {trainer.last_name}
+			</div>
+		);
+	});
+
+	return (
+		<div>
+			<div>{renderedProducts}</div>
+			<br />
+			<div>{renderedMembers}</div>
+			<br />
+			<div>{renderedTrainers}</div>
+		</div>
+	);
 }
 
 export default UserPage;
