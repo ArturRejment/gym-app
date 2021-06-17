d99be511653502eb5fc80ff403ad576235d78f4a
Artur Rejment <arturrejment1@gmail.com>
2021-06-17 12:55:38 +0200

    Receptionist page style and functionality

--- full-stack/myapp/urls.py
+++ full-stack/myapp/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("client/", views.client, name="client"),
-    path("trainerUpdate/", views.trainerUpdate, name="trainerUpdate"),
-    path("receptionist/", views.receptionist, name="receptionist"),
     path("", views.index, name="index"),
+
     path("trainerPage/", views.trainerPage, name="trainerPage"),
+    path("trainerUpdate/", views.trainerUpdate, name="trainerUpdate"),
+    path("client/", views.client, name="client"),
+    path("receptionist/", views.receptionist, name="receptionist"),
+
     path("login/", views.loginPage, name="login"),
     path("logout/", views.logoutPage, name="logout"),
 ]
